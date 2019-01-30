f7e59c498b286f404b3d75f89039ed707d57551c
Pauline <pbaker@hotmail.co.uk>
2019-01-30 15:34:04 +0000

    added unit tests for user validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,26 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
+  subject { described_class.new(password: "password", email: "test@example.com", user_name: "Name") }
+
+  describe "Validations" do
+    it "is valid with valid attributes" do
+      expect(subject).to be_valid
+    end
+
+    it "is not valid without a password" do
+      subject.password = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid without an email" do
+      subject.email = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid without a username" do
+      subject.user_name = nil
+      expect(subject).to_not be_valid
+    end
+  end
 end
